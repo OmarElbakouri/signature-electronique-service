0b93e4ab7bcb4fb36a4dd4ea4fcf2229b63ed119
nesrin <aboumahfoudnisrine@gmail.com>
2025-07-20 12:13:09 +0100

    Initial commit: Electronic Signature Service project structure

---
+++ src/test/java/com/signature/electronic_signature_service/ElectronicSignatureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signature.electronic_signature_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicSignatureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

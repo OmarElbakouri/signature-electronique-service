ae2dcf303f705fb5929e62b96496dd999c285bbe
OmarElbakouri <elbakouriomar45@gmail.com>
2025-07-24 10:13:14 +0100

    modification

---
+++ proj/signature-electronique-service/src/main/java/com/signature/electronic_signature_service/ElectronicSignatureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.signature.electronic_signature_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicSignatureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicSignatureServiceApplication.class, args);
+	}
+
+}
